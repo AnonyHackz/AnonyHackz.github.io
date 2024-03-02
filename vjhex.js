b7cfba64f8e88f02e161f26a3c71372fab2071cd
vjveerappa <vijayveerappa741@gmail.com>
2024-03-02 14:46:53 +0000

    v1
    m

--- vjhex.js
+++ vjhex.js
@@ -52,7 +52,7 @@ function check(){
         document.getElementById("intro").innerHTML=" Hereby we are thrilled to inform you that we have multiple courses available for you to pursue  in the field of IT.  </p>"   ;                                                      
         
                                                             // To append html codes into created element
-        pro.innerHTML+='<form><table><td><ol>   <li><a>HTML</a></li>  <li><a>JavaScript</a></li>  <li><a>CSS</a></li> <li><a>SQL</a></li> <li><a>Python</a></li> <li><a href="../templates/EthicHackz.html" targrt="blank">Ethical Hacking</a></li> <li><a>Machine Learning</a></li>  <li><a>Java</a></li>    <li><a>AutoCad</a></li>  <li><a>Web Development</a></li>  <li><a>Reverse Engineering</a></li>  </ol></td>  </table><button id="bt2" onclick="home()">Previous Page</button></form>';
+        pro.innerHTML+='<form><table><td><ol>   <li><a>HTML</a></li>  <li><a>JavaScript</a></li>  <li><a>CSS</a></li> <li><a>SQL</a></li> <li><a>Python</a></li> <li><a href="EthicHackz.html" targrt="blank">Ethical Hacking</a></li> <li><a>Machine Learning</a></li>  <li><a>Java</a></li>    <li><a>AutoCad</a></li>  <li><a>Web Development</a></li>  <li><a>Reverse Engineering</a></li>  </ol></td>  </table><button id="bt2" onclick="home()">Previous Page</button></form>';
       
       
         var con = document.getElementById("container");    // To append created element into html file
