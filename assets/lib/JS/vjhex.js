da06a6f8eee0f740cf4765f204a47127d0dae3df
vjveerappa <vijayveerappa741@gmail.com>
2024-12-08 18:07:22 +0000

    V5
    Updated on 08/12/2024

--- assets/lib/JS/vjhex.js
+++ assets/lib/JS/vjhex.js
@@ -25,7 +25,7 @@
     
       con.removeChild(cenbt);
       var btn = document.getElementById("btn");
-      con.removeChild(btn);
+      
   }
 
   
@@ -52,18 +52,18 @@
         document.getElementById("intro").innerHTML=" Hereby we are thrilled to inform you that we have multiple courses made available for you to pursue  in the field of IT."   ;                                                      
         
                                                             // To append html codes into created element
-        pro.innerHTML+='                         <div id="prj"><img id="im" src="Common/Media/Background/BG5.jpg"><a  onclick="Soon()"><h3 id="prclk">HTML</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>JavaScript</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()"><h3>CSS</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>SQL</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>Python</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" href="EthicHackz.html" ><h3>Cyber Security </h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>Machine Learning</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>Java</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" href="CloudComputing.html"  ><h3>Cloud Computing</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>Web Development</h3></a></div> \
-                                                 <div id="prj"><img id="im" src=""><a id="prclk" onclick="Soon()" ><h3>Reverse Engineering</h3></a></div> \
-                        ';
+        pro.innerHTML+=`                         <div id="prj" onclick="Soon()" ><img id="im" src="HTML/Icons/icon.png"><lable id="prclk"  >HTML</lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="JavaScript/Icons/icon.png"><lable id="prclk" >JavaScript</lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="CSS/Icons/icon.png"><lable id="prclk" >CSS</lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="SQL/Icons/icon.png"><lable id="prclk" >SQL</lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="Python/Icons/icon.png"><lable id="prclk" >Python</lable></a></div> 
+                                                 <div id="prj" onclick="window.location.href='assets/lib/HTML/EthicHackz.html';" ><img id="im" src="Cyber Security/Icons/icon.png"><lable id="prclk" >Cyber Security </lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="Machine Learning/Icons/icon.png"><lable id="prclk" >Machine Learning</lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="Java/Icons/icon.png"><lable id="prclk" >Java</lable></a></div> 
+                                                 <div id="prj" onclick="window.location.href='assets/lib/HTML/CloudComputing.html';"><img id="im" src="Cloud Computing/Icons/icon.png">   <lable id="prclk" >Cloud Computing</lable></a></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="Web Development/Icons/icon.png"><lable id="prclk" >Web Development</lable></div> 
+                                                 <div id="prj" onclick="Soon()" ><img id="im" src="Reverse Engineering/Icons/icon.png"><lable id="prclk" >Reverse Engineering</lable></div> 
+                        `;
        
         bt2.innerHTML+= '<center><button id="bt2" onclick="home()">Previous Page</button></center>';
         var con = document.getElementById("container");    // To append created element into html file
@@ -88,6 +88,8 @@
     
         var con = document.getElementById("container");
         var pr = document.getElementById("project");
+        var btn=document.getElementById("btn");
+        con.removeChild(btn);
         con.removeChild(pr);
         con.innerHTML += `<button id="bt" onclick="he()">Let's have a look</button>`;
     
